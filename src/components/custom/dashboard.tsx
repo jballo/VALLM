fa9532f5acee6d2e464398ef56a5d941870db4f2
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-03-27 17:08:02 -0700

    Feat: Removed unecessary 'textComparison.tsx'

--- src/components/custom/dashboard.tsx
+++ src/components/custom/dashboard.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Header from "./header";
 import PromptForm from "./promptform";
-// import TextComparison from "./textComparison";
 import LLMResponseComparison from "./llm-response-comparison";
 
 interface LLMResponses {
@@ -29,20 +28,17 @@ interface CreateEmbeddingProp {
 interface DashboardProps {
   createResponse: CreateResponseProps["createResponse"];
   createEmbedding: CreateEmbeddingProp["createEmbedding"];
-//   ragRetrieval: RagProps["ragRetrieval"];
 }
 
 
 
 export default function Dashboard({createResponse, createEmbedding}: DashboardProps) {
     const [url, setUrl] = useState<string>("");
-    // const [responses, setResponses] = useState<LLMResponses[]>([]);
 
     return (
         <div className="w-full min-h-screen p-12 flex flex-col gap-6">
               <Header />
               <PromptForm createEmbedding={createEmbedding} url={url} setUrl={setUrl}/>
-              {/* <TextComparison responses={responses} /> */}
               <LLMResponseComparison url={url} createResponse={createResponse}/>
         </div>
     );
