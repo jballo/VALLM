35c0831d88530056082913a60a75d93c96b2e579
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-04-12 20:17:24 -0700

    Feat: Prompt Form Update
    Changed color scheme to future theme

--- src/components/custom/promptform.tsx
+++ src/components/custom/promptform.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Globe } from "lucide-react";
 // import { useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
@@ -52,16 +53,27 @@ export default function PromptForm( { createEmbedding, url, setUrl }: PromptForm
         }
     }
     return (
-        <div className="flex flex-col gap-4 w-full">
-            <div className="w-full">
-                <p>URL</p>
+        <div className="flex flex-col w-full bg-[#0e2232] text-white p-3 rounded-xl">
+            <div className="w-full flex flex-col gap-2">
+                <div className="flex flex-row gap-6">
+                    <div className="flex flex-row gap-1">
+                        <Globe  className="text-[#36c5b3] w-5"/>
+                        <p className="text-md"> URL Scraper</p>
+                    </div>
+                    <div className="flex flex-row items-end mb-0.5">
+                        <p className="text-xs text-muted-foreground">Scrape content from a URL to use as context for all test cases</p>
+                    </div>
+                </div>
                 <div className="flex flex-row gap-3">
                     <Input 
                         type="text"
                         value={url}
+                        className="bg-[#213342] text-[#b1b7bd] border-[#3F4E5D]"
+                        placeholder="https://example.com/page-to-scrape"
                         onChange={e => setUrl(e.target.value)}
                     />
                     <Button
+                        className="bg-[#36c5b3]"
                         onClick={submitUrl}
                     >
                         Submit
