df826534bc0cb79d37edc3540fade6d0dbf18189
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-04-16 16:45:30 -0700

    Feat: Add test case button works for sidebar
    Note: Rest of functionality still needs to be implemented connected to test cases

--- src/components/custom/dashboard.tsx
+++ src/components/custom/dashboard.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./header";
 import PromptForm from "./promptform";
 import LLMResponseComparison from "./llm-response-comparison";
 import Sidebar from "./sidebar";
+import { v4 as uuidv4 } from "uuid";
 
+
+interface TestCase {
+  id: string;
+  prompt: string;
+  expectedOutput: string;
+}
 
 interface CreateEmbeddingProp {
     createEmbedding: (
@@ -22,12 +29,34 @@ interface DashboardProps {
 
 export default function Dashboard({ createEmbedding}: DashboardProps) {
     const [url, setUrl] = useState<string>("");
+    const [testCases, setTestCases] = useState<TestCase[]>([]);
+    const [currentTestCase, setCurrentTestCase] = useState<string>("");
+
+    const addTestCase = () => {
+        const id = uuidv4();
+        // setTestCases([...testCases, { id: id, prompt: "", expectedOutput: "" }]);
+        setTestCases((prev) => [...prev, { id: id, prompt: "", expectedOutput: "" }]);
+        // setTabVal(id);
+        setCurrentTestCase(id);
+    };
+
+    useEffect(() => {
+        addTestCase();
+    }, []);
+    
+    useEffect(() => {
+        console.log("Test cases: ", testCases);
+    }, [testCases]);
+
+    useEffect(() => {
+        console.log("Current test case: ", currentTestCase);
+    }, [currentTestCase]);
 
     return (
         <div className="w-full min-h-screen p-12 flex flex-col gap-6">
               <Header />
               <div className="flex flex-row h-full">
-                <Sidebar />
+                <Sidebar testCases={testCases} addTestCase={addTestCase} setCurrentTestCase={setCurrentTestCase}/>
                 <div className="w-full">
                     <PromptForm createEmbedding={createEmbedding} url={url} setUrl={setUrl}/>
                     <LLMResponseComparison 
