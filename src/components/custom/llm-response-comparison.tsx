3ad55d32e88f8423415b31f7f89e0f5be0a9e96a
Jonathan B <31107158+jballo@users.noreply.github.com>
2025-04-20 23:04:27 -0700

    Feat: Container takes full width for test case editor and models to compare

--- src/components/custom/llm-response-comparison.tsx
+++ src/components/custom/llm-response-comparison.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import TestCaseInput from "./test-case-input";
 // import Results from "./results";
-import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { Stars, Zap } from "lucide-react";
 import { Button } from "../ui/button";
 
@@ -13,6 +13,16 @@ interface TestCase {
   prompt: string
   expectedOutput: string
 }
+
+// interface LLMResponses {
+//     llm_name: string;
+//     llm_response: string;
+//     contextual_relevancy_score: number;
+//     answer_relevancy_score: number;
+//     bias_success_score: number;
+//     toxicity_success_score: number;
+//     correctness_success_score: number;
+// }
 
 interface CreateResponseProps {
   // url: string;
@@ -26,6 +36,7 @@ interface CreateResponseProps {
 export default function LLMResponseComparison({ testCases, currentTestCase, updateTestCase }: CreateResponseProps) {
   // const [submittedTests, setSubmittedTests] = useState<TestCase[]>([]);
   const [resultTab, setResultTab] = useState<string>("");
+  // const [results, setResults] = useState<LLMResponses[]>([]);
 
   const onTabChange = (value: string) => {
     setResultTab(value);
@@ -37,35 +48,54 @@ export default function LLMResponseComparison({ testCases, currentTestCase, upda
     }
   }, [resultTab]);
 
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   const validTestCases = testCases.filter(
+  //     (tc) => tc.prompt.trim() && tc.expectedOutput.trim()
+  //   );
+  //   if (validTestCases.length > 0) {
+  //     // onSubmit(validTestCases);
+  //     console.log("Valid test cases: ", validTestCases);
+  //     // setSubmittedTests([]);
+  //     setSubmittedTests(validTestCases);
+  //     setResultTab(validTestCases[0].id);
+  //     const id = uuidv4();
+  //     setTestCases([{ id: id, prompt: "", expectedOutput: "" }]);
+  //     setTabVal(id);
+  //   }
+  // };
+
   return (
     <div className="container mx-auto p-4 space-y-8">
-      <Card className="bg-[#011627]">
-        <CardHeader>
-          <div className="flex flex-row justify-between">
-            <Tabs defaultValue="editor">
+      <div className="bg-[#011627] rounded-3xl">
+          <div className="flex flex-row justify-between p-10">
+            <Tabs defaultValue="editor" className="w-full">
               <TabsList className="bg-[#152838]">
                 <TabsTrigger value="editor" className="data-[state=active]:bg-[#36c5b3] data-[state=active]:text-white"><Stars />Editor</TabsTrigger>
                 <TabsTrigger value="results" className="data-[state=active]:bg-[#36c5b3] data-[state=active]:text-white"><Zap />Results</TabsTrigger>
               </TabsList>
+              <TabsContent value="editor" className="min-w-screen">
+                <TestCaseInput
+                  // setSubmittedTests={setSubmittedTests}
+                  // setResultTab={setResultTab}
+                  testCases={testCases}
+                  currentTestCase={currentTestCase}
+                  updateTestCase={updateTestCase}
+                />
+              </TabsContent>
+              <TabsContent value="results">
+                <p>Results</p>
+              </TabsContent>
             </Tabs>
             <Button
               variant="ghost"
-              className="text-white bg-[#36c5b3] hover:bg-[#278f81]"
+              className="text-white bg-[#36c5b3] hover:bg-[#278f81] ml-[-120px]"
             >
               Run Test{(testCases.length > 1) && (<>s ({testCases.length})</>)}
             </Button>
           </div>
-        </CardHeader>
-        <CardContent>
-          <TestCaseInput
-            // setSubmittedTests={setSubmittedTests}
-            // setResultTab={setResultTab}
-            testCases={testCases}
-            currentTestCase={currentTestCase}
-            updateTestCase={updateTestCase}
-          />
-        </CardContent>
-      </Card>
+      </div>
       {/* {submittedTests.length > 0 && resultTab.length > 0 && (
         <Card>
           <CardHeader>
